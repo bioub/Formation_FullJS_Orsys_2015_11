7d7c11dd85648474351fd20f4c5060817a74ec4c
bioub <romain.bohdanowicz@gmail.com>
2015-11-03 12:24:02 +0100

    Fin grunt

--- TachesNode/Gruntfile.js
+++ TachesNode/Gruntfile.js
@@ -7,22 +7,59 @@ module.exports = function(grunt) {
     });
 
     grunt.initConfig({
-        useminPrepare: 'public/*.html',
-        //copy: {
-        //    dist: {
-        //        expand: true,
-        //        cwd: 'public',
-        //        src: ['*.{js,css,html}'],
-        //        dest: 'dist/'
-        //    },
-        //},
+        useminPrepare: {
+            html: 'public/*.html',
+        },
+        usemin: {
+            html: 'dist/*.html'
+        },
+        rev: {
+            assets: {
+                files: [{
+                    src: [
+                        'dist/**/*.{css,js}',
+                    ]
+                }]
+            }
+        },
+        copy: {
+            dist: {
+                expand: true,
+                cwd: 'public',
+                src: ['*.html'],
+                dest: 'dist/'
+            },
+        },
         clean: {
             dist: ["dist/*"],
         },
+        cdnjquery: {
+            dist: {}
+        }
+    });
+
+    grunt.registerMultiTask('cdnjquery', 'Passe de bower à code.jquery.com', function() {
+        var htmlfile = 'dist/index.html';
+
+        var srcContent = '../bower_components/jquery/dist/jquery.js';
+        var destContent = '//code.jquery.com/jquery-1.11.3.min.js';
+
+        var contenu = grunt.file.read(htmlfile);
+
+        contenu = contenu.replace(srcContent, destContent);
+
+        grunt.file.write(htmlfile, contenu);
     });
 
     grunt.registerTask('default', [
         'clean:dist',
-        'copy:dist'
+        'useminPrepare',
+        'concat:generated',
+        'uglify:generated',
+        'cssmin:generated',
+        'rev',
+        'copy:dist',
+        'usemin',
+        'cdnjquery'
     ]);
 }
